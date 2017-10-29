494b01c5b39993d240be256005623b5954abba37
nkinta <nkinta1978+github@gmail.com>
2017-10-29 09:17:55 +0900

    汎用化

--- picture_webserver/batch/run_server.bat
+++ picture_webserver/batch/run_server.bat
@@ -1,5 +1,5 @@
 pushd ..\..\
-set VENVBAT="C:\Users\nkinta\Documents\python\env\picture_server\Scripts\activate.bat"
+set VENVBAT="C:\Users\%username%\Documents\python\env\picture_server\Scripts\activate.bat"
 call %VENVBAT%
 python -m picture_webserver.server_main
 popd

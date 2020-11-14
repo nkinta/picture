c89de0d9045f064d60dbd9441825ec47750991aa
nkinta <nkinta1978+github@gmail.com>
2020-11-15 01:47:35 +0900

    daitai kansei

--- get_pic_yz/main.py
+++ get_pic_yz/main.py
@@ -32,9 +32,11 @@ pyocr.tesseract.TESSERACT_CMD = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 COOKIE_FILE_PATH = os.path.join(settings.PICTURE_TEMP_PATH, "cookie.txt")
 
-JPEG_COOKIE = "CloudFront-Key-Pair-Id=APKAJZ4XANN3SJSZ5TOA; _gid=GA1.2.845150499.1557673672; _ga=GA1.2.421634; _gat=1; CloudFront-Expires=1557843191; CloudFront-Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9jZG4uaW1hZ2UuODEyMi5qcC92MS8qLzgxMjJqcC8qIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNTU3ODQzMTkxfX19XX0_; CloudFront-Signature=Rl0Wu82VvP1QEpL7hd%7ELhlanwmBlNTPIYodG6wn1NhJH3Ea-V2EaqMkM%7E93uw9%7EU-IkV6Qn3pUvrxgKJdYWHuEDKBu622%7ENwvUWd0jZg-t0VY23-8BAQLO6uLTLBh8L2aOAxe1bXgBQt3bfpgRKYEbY8sySQGK%7Eg5L2BChG%7E1bCNHuAYTqgqf-UBEZX4SBrDJXvwVsazmEvGk-%7Ez6a4y6iWuJ2Pvxh7A9LvGRKgOPR6rQ3bq6Zf4XI6nd8ml-5umo0gy6WKO9%7E7RcPfA68%7ED9sK02OSWZ1xXP51S6xEYIC9bUI1WRRcgu64szJiwtkmh3gjOq9-i27tEu7N10E0VWg__"
+PAGE_NUM = 20
 
-GROUP_ID = 1762
+
+class Error(Exception):
+    pass
 
 
 def add_default_header(req):
@@ -82,15 +84,21 @@ def get_image_str(root_image_file_path):
     return result
 
 
-def get_root_html(opener):
-
+@utility.try_again(3, 5.0)
+def get_root_main(opener, file_path):
     req = urllib.request.Request(settings.URL_ROOT)
     add_default_header(req)
-    res = opener.open(req)
+    with opener.open(req) as res:
+        data = res.read()
+
+    with open(file_path, mode="wb") as f:
+        f.write(data)
+
+
+def get_root_html(opener):
 
     root_html_file_path = os.path.join(settings.PICTURE_TEMP_PATH, "root_html.html")
-    with open(root_html_file_path, mode="wb") as f:
-        f.write(res.read())
+    get_root_main(opener, root_html_file_path)
 
     with open(root_html_file_path, mode="r", encoding='utf-8') as f:
         soup = BeautifulSoup(f.read(), "html.parser")
@@ -98,14 +106,15 @@ def get_root_html(opener):
     login_image_url = soup.find("img", {"class": "top-security-code-img"})["src"]
     req = urllib.request.Request(login_image_url)
     add_default_header(req)
-    res = opener.open(req)
 
-    root_image_file_path = os.path.join(settings.PICTURE_TEMP_PATH, "root_html.jpg")
+    with opener.open(req) as res:
+        root_image_file_path = os.path.join(settings.PICTURE_TEMP_PATH, "root_html.jpg")
 
-    with open(root_image_file_path, mode="wb") as f:
-        f.write(res.read())
+        with open(root_image_file_path, mode="wb") as f:
+            f.write(res.read())
 
     phrase = get_image_str(root_image_file_path)
+    print("phrase -> ", phrase)
 
     return phrase
 
@@ -129,33 +138,36 @@ def login_main(opener, login_id, password, phrase):
 
     print("url_login", url_login)
     data = urllib.parse.urlencode(post).encode('utf-8')
+    print("phrase -> ", phrase)
     with opener.open(req, data) as res:
         result = res.read()
+        print(result)
 
 
-# @utility.try_again(3, 5.0)
-def get_photo_param(opener, photo_no):
+@utility.try_again(3, 5.0)
+def get_photo_param(opener, group_id, photo_no):
 
-    GROUP_ID = 1762
+    file_path = os.path.join(settings.PICTURE_TEMP_PATH, "g_{}".format(group_id), "photo_param", "photo_{}.json".format(photo_no))
 
-    file_path = os.path.join(settings.PICTURE_TEMP_PATH, "photo_{}.json".format(photo_no))
-
-    if not os.path.isfile(file_path):
+    if True:  # not os.path.isfile(file_path):
 
         get_photo_url = "{}{}".format(settings.URL_ROOT, r"api/photo/get/photo.html")
 
+        page_id = (photo_no - 1) // PAGE_NUM + 1
+
         post = {
             'photoListGetType': "FRONT_GROUP_DETAIL_GROUP_ID",
-            'pageId': 1,
-            "photoId": "{:0=4}::{:0=4}.jpg".format(GROUP_ID, photo_no),
+            'pageId': page_id,
+            "photoId": "{:0=4}::{:0=4}.jpg".format(group_id, photo_no),
         }
 
         req = urllib.request.Request(get_photo_url)
         add_default_header(req)
-        req.add_header('Referer', "{}{}".format(settings.URL_ROOT, r"front/group/change.html?nextAction=front_group_detail&group_id=1762&thumbnail_type=230"))
+        req.add_header('Referer', "{}{}".format(settings.URL_ROOT, r"front/group/change.html?nextAction=front_group_detail&group_id={}&thumbnail_type=230").format(group_id))
         data = urllib.parse.urlencode(post).encode('utf-8')
 
         with opener.open(req, data) as res:
+            utility.make_directory(file_path)
             with open(file_path, mode="wb") as f:
                 f.write(res.read())
 
@@ -179,15 +191,25 @@ def get_photo_param(opener, photo_no):
 
     return photo_key_enc, onetime_key
 
-# @utility.try_again(3, 5.0)
+
+def get_base64_json_path(group_id, photo_no):
+    json_path = os.path.join(settings.PICTURE_TEMP_PATH, "g_{}".format(group_id), "pic_json", "photo_base64_{}.json".format(photo_no))
+
+    return json_path
 
 
-def get_generator(opener, photo_no, photo_key_enc, onetime_key):
+def get_split_jpg_path(group_id, photo_no, split_index):
+    file_path = os.path.join(settings.PICTURE_TEMP_PATH, "g_{}".format(group_id), "pic_temp", "photo_base64_{}_{}.jpg".format(photo_no, split_index))
 
-    file_path = os.path.join(settings.PICTURE_TEMP_PATH, "photo_gen_{}.gzip".format(photo_no))
-    file_base64_json_path = os.path.join(settings.PICTURE_TEMP_PATH, "photo_base64_{}.json".format(photo_no))
+    return file_path
 
-    if not os.path.isfile(file_path):
+
+def get_generator(opener, group_id, photo_no, photo_key_enc, onetime_key):
+
+    file_path = os.path.join(settings.PICTURE_TEMP_PATH, "g_{}".format(group_id), "pic_json", "photo_gen_{}.gzip".format(photo_no))
+    file_base64_json_path = get_base64_json_path(group_id, photo_no)
+
+    if True:  # not os.path.isfile(file_path):
 
         get_photo_url = "{}{}".format(settings.URL_ROOT, r"api/photo/generator.html")
 
@@ -201,7 +223,7 @@ def get_generator(opener, photo_no, photo_key_enc, onetime_key):
         }
 
         req = urllib.request.Request(get_photo_url)
-        req.add_header('Referer', "{}{}".format(settings.URL_ROOT, r"front/group/change.html?nextAction=front_group_detail&group_id=1762&thumbnail_type=230"))
+        req.add_header('Referer', "{}{}".format(settings.URL_ROOT, r"front/group/change.html?nextAction=front_group_detail&group_id={}&thumbnail_type=230").format(group_id))
         add_default_header(req)
         # req.add_header("Accept-Encoding", "gzip, deflate, br")
         # req.add_header("Content-Type", "application/json;charset=UTF-8")
@@ -213,11 +235,13 @@ def get_generator(opener, photo_no, photo_key_enc, onetime_key):
         from gzip import GzipFile
 
         with opener.open(req, data) as res:
+            utility.make_directory(file_path)
             with open(file_path, mode="wb") as f:
                 new_data = res.read()
                 f.write(new_data)
 
             with open(file_path, mode="rb") as fr:
+                utility.make_directory(file_base64_json_path)
                 with open(file_base64_json_path, mode="wb") as fw:
                     with GzipFile(fileobj=fr, mode='rb') as gf:
                         fw.write(gf.read())
@@ -230,28 +254,18 @@ def get_generator(opener, photo_no, photo_key_enc, onetime_key):
 
     import base64
 
+    jpg_path_list = []
     for i, base64_data in enumerate(json_object["data"]):
-        file_base64_json_path = os.path.join(settings.PICTURE_TEMP_PATH, "photo_base64_{}_{}.jpg".format(photo_no, i))
-        with open(file_base64_json_path, "wb") as f:
+        file_base64_jpg_path = get_split_jpg_path(group_id, photo_no, i)
+        jpg_path_list.append(file_base64_jpg_path)
+        utility.make_directory(file_base64_jpg_path)
+        with open(file_base64_jpg_path, "wb") as f:
             main_base64_data = base64_data[23:]
             f.write(base64.b64decode(main_base64_data))
 
-    """
-    json_data = None
-    if os.path.isfile(file_path):
-        with open(file_path, mode="r") as f:
-            try:
-                json_data = json.loads(f.read())
-            except:
-                print("json read error ({})".format(file_path))
-    """
-
-    # pprint.pprint(json_data)
-
-    return
+    return jpg_path_list, json_object
 
 
-# @utility.try_again(3, 5.0)
 def get_category_list(opener, page_no):
 
     file_path = os.path.join(settings.PICTURE_TEMP_PATH, "change_{}.html".format(page_no))
@@ -287,173 +301,57 @@ def get_category_list(opener, page_no):
         img_element = photo_element.find("a").find("img")
         print(img_element["onload"])
 
-    """
-    category_no_tag_list = soup.find_all("li", {"class": "p-categoryList_category"})
-    event_info = soup.find("span", {"class": "p-eventInfo_content"})
-
-
-    date_kanji = event_info.text
-
-    year_month_date = get_date(date_kanji)
-
-    category_no_list = [category_no_tag["data-categoryno"] for category_no_tag in category_no_tag_list]
-
-
-    class CategoryParser(html_parser.HTMLParser):
-
-        def handle_starttag(self, tag, attrs):  # 開始タグを扱うためのメソッド
-            if tag == "li":
-                attr_dict = dict(attrs)
-                category_no = attr_dict.get("data-categoryno", None)
-                if category_no:
-                    category_no_list.append(category_no)
-
-    parser = CategoryParser()
-
-    with open(file_path, mode="r", encoding='utf-8') as f:
-        parser.feed(str(f.read()))
-
-    parser.close()
-    """
-
     # return category_no_list, year_month_date
 
-
-@utility.try_again(3, 5.0)
-def get_picture_list(opener, eventno, categoryno, offset=0):
-
-    file_path = os.path.join(settings.PICTURE_TEMP_PATH, "json_data", eventno, "category_picture_list_{}_{}.json".format(categoryno, offset))
-
-    json_data = None
-    if os.path.isfile(file_path):
-        with open(file_path, mode="r") as f:
-            try:
-                json_data = json.loads(f.read())
-            except:
-                print("json read error ({})".format(file_path))
-
-    if json_data is None:
-        parse_result = urllib.parse.urlparse(settings.URL_ROOT)
-
-        query_dict = {
-            'action_Api_FastViewer_PhotoList': 'true',
-            'eventno': eventno,
-            'categoryno': categoryno,
-            'offset': offset,
-            'page': 'photolist',
-            'sortkey': '0',
-        }
-
-        scheme, netloc, path, params, _, fragment = parse_result
-        query = urllib.parse.urlencode(query_dict)
-
-        url = urllib.parse.urlunparse((scheme, netloc, path, params, query, fragment))
-
-        print(url)
-
-        req = urllib.request.Request(url)
-        referer_url = '{}/?action_user_FastViewer=t&eventno={}'.format(settings.URL_ROOT, eventno)
-        req.add_header('Referer', referer_url)
-        req.add_header('accept', 'application/json, text/javascript, */*; q=0.01')
-        req.add_header('accept-language', settings.HTTP_HEADER_LANGUAGE)
-        req.add_header('User-Agent', settings.HTTP_HEADER_USER_AGENT)
-
-        res = opener.open(req)
-
-        utility.make_directory(file_path)
-        with open(file_path, mode="wb") as f:
-            f.write(res.read())
-
-        with open(file_path, mode="r") as f:
-            json_data = json.loads(f.read())
-
-    json_data_list = []
-    json_data_list.append(json_data)
-
-    if json_data["message"]["pager"]["hasnext"]:
-        next_json_data_list = get_picture_list(opener, eventno, categoryno, offset=offset + 100)
-        json_data_list += next_json_data_list
-
-    return json_data_list
+COMBINE_V, COMBINE_H = range(2)
 
 
-@utility.try_again(3, 5.0)
-def download_pic(opener, eventno, url, file_path):
+def combine_file(file_path_list, target_file_path, mergin, v_or_h):
 
-    print("url-> ", url)
-    print("file_path-> ", file_path)
+    if v_or_h == COMBINE_V:
+        height_name = "height"
+        width_name = "width"
+    elif v_or_h == COMBINE_H:
+        height_name = "width"
+        width_name = "height"
 
-    if opener is None:
-        return
+    def calc_total_height(height_name, width_name):
+        total_height = 0
+        width = getattr(im_list[0], width_name)
+        for im in im_list:
+            total_height = total_height + getattr(im, height_name)
+            if width != getattr(im, width_name):
+                raise Error("width difference {} != {}".format(width, getattr(im, width_name)))
+        return total_height, width
 
-    req = urllib.request.Request(url)
-    referer_url = '{}/?action_user_FastViewer=t&eventno={}'.format(settings.URL_ROOT, eventno)
-    req.add_header('Referer', referer_url)
-    req.add_header('User-Agent', settings.HTTP_HEADER_USER_AGENT)
-    req.add_header('accept-encoding', 'gzip, deflate, br')
-    req.add_header('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3')
-    req.add_header('cache-control', 'max-age=0')
-    # req.add_header('cookie', JPEG_COOKIE)
-    jpg = opener.open(req).read()
+    im_list = []
+    for file_path in file_path_list:
+        im_list.append(Image.open(file_path))
 
-    utility.make_directory(file_path)
-    with open(file_path, mode="wb") as f:
-        f.write(jpg)
+    mergin_total = mergin * (len(file_path_list) - 1)
 
+    total_height, width = calc_total_height(height_name, width_name)
 
-def combine_file(combine_file_path, pic_file_path_0, pic_file_path_1):
+    if v_or_h == COMBINE_V:
+        target_image = Image.new("RGB", (width, total_height - mergin_total))
+    elif v_or_h == COMBINE_H:
+        target_image = Image.new("RGB", (total_height - mergin_total, width))
 
-    def _get_mask_image(target_image_size):
-        for ref_image_size in ((1200, 800), (1124, 800), (800, 1154), (800, 1200), (1116, 800), (1187, 800), (1167, 800)):
-            if abs((target_image_size[0] / target_image_size[1]) - (ref_image_size[0] / ref_image_size[1])) < 0.02:
-                mask_image = Image.open(os.path.join(settings.MASK_DIRECTORY_PATH, "mask_{}_{}.bmp".format(*ref_image_size))).convert("1").resize(target_image_size)
-                return mask_image
+    current_height = 0
+    for im in im_list:
+        if v_or_h == COMBINE_V:
+            target_image.paste(im, (0, current_height))
+        elif v_or_h == COMBINE_H:
+            target_image.paste(im, (current_height, 0))
 
-        return None
+        current_height = current_height + getattr(im, height_name) - mergin
 
-    image_0 = Image.open(pic_file_path_0)
-    image_1 = Image.open(pic_file_path_1)
-
-    mask_image = _get_mask_image(image_0.size)
-
-    print(pic_file_path_0, image_0.size)
-
-    if (image_0.size == image_1.size == mask_image.size):
-        out_image = Image.composite(image_1, image_0, mask_image)
-
-        print(combine_file_path, out_image.size)
-        utility.make_directory(combine_file_path)
-        out_image.save(combine_file_path)
-
-        # out_image.show()
+    utility.make_directory(target_file_path)
+    target_image.save(target_file_path)
+    # out_image.show()
 
 
-def download_pics(opener, eventno, categoryno, offset, json_data, year_month_date):
-
-    for pic_info in json_data["message"]["photos"]:
-
-        url_data_list = [pic_info["m_pclogo1"], pic_info["m_pclogo2"]]
-        sufix_list = ["1", "2"]
-        year_month_date_num = [int(v) for v in year_month_date]
-        cache_file_path_list = [os.path.join(settings.PICTURE_TEMP_PATH, "cache_pic_data", eventno, categoryno, str(offset), "{}_{}.jpg".format(pic_info["n"], s)) for s in sufix_list]
-        # vsave_file_path_list = [os.path.join(settings.PICTURE_TEMP_PATH, "pic_data", , categoryno, str(offset), "{}_{}.jpg".format(pic_info["n"], s)) for s in sufix_list]
-
-        for url_data, cache_file_path in zip(url_data_list, cache_file_path_list):
-            if not os.path.exists(cache_file_path):
-                download_pic(opener, eventno, url_data, cache_file_path)
-
-                time.sleep(random.random())
-
-        combine_file_path = os.path.join(settings.PICTURE_TEMP_PATH, "combine_data", "{}_{:04}_{:02}{:02}".format(eventno, *year_month_date_num), categoryno, str(offset), "{}.jpg".format(pic_info["n"]))
-
-        combine_file(combine_file_path, cache_file_path_list[0], cache_file_path_list[1])
-
-        override_date(combine_file_path, year_month_date)
-        # utility.make_directory(save_file_path)
-        # shutil.copy(cache_file_path, save_file_path)
-
-
-def login(login_id, password, local_test_flag):
+def login(login_id, password, local_test_flag, do_login_flag):
 
     if local_test_flag:
         return None
@@ -464,9 +362,9 @@ def login(login_id, password, local_test_flag):
     cookie_processor = urllib.request.HTTPCookieProcessor(cookie_jar)
     opener = urllib.request.build_opener(cookie_processor)
 
-    phrase = get_root_html(opener)
-
-    login_main(opener, login_id, password, phrase)
+    if do_login_flag:
+        phrase = get_root_html(opener)
+        login_main(opener, login_id, password, phrase)
 
     return opener
 
@@ -508,17 +406,74 @@ def override_date(file_path, year_month_date):
     change_time.set_time(file_path, int(year), int(month), int(date), 12 + hour, minute, sec, 0)
 
 
+def get_result_file_path(group_id, photo_no):
+    file_path = os.path.join(settings.PICTURE_TEMP_PATH, "g_{}".format(group_id), "picture", "photo_{}.jpg".format(photo_no))
+
+    return file_path
+
+
+def get_split_dir(json_object):
+
+    combine_direction = COMBINE_V
+    if json_object["splits"][0] == 1:
+        combine_direction = COMBINE_V
+    elif json_object["splits"][1] == 1:
+        combine_direction = COMBINE_H
+
+    return combine_direction
+
+
+def download_pic(opener, group_id, start_no, end_no):
+
+    for photo_no in range(start_no, end_no):
+        file_path = get_result_file_path(group_id, photo_no)
+        photo_key_enc, onetime_key = get_photo_param(opener, group_id, photo_no)
+        div_file_path_list, json_object = get_generator(opener, group_id, photo_no, photo_key_enc, onetime_key)
+
+        combine_direction = get_split_dir(json_object)
+
+        combine_file(div_file_path_list, file_path, 5, combine_direction)
+
+
+def combine_only(group_id, start_no, end_no):
+
+    target_override_date = (2020, 10, 5)
+
+    for photo_no in range(start_no, end_no):
+        json_path = get_base64_json_path(group_id, photo_no)
+        with open(json_path, mode="r") as f:
+            json_object = json.load(f)
+        num = len(json_object["data"])
+
+        div_file_path_list = []
+        for split_index in range(num):
+            div_file_path_list.append(get_split_jpg_path(group_id, photo_no, split_index))
+        file_path = get_result_file_path(group_id, photo_no)
+
+        combine_direction = get_split_dir(json_object)
+
+        combine_file(div_file_path_list, file_path, 5, combine_direction)
+
+        override_date(file_path, target_override_date)
+
+
 def main():
+    group_id = 1762
+
+    combine_only(group_id, 1, 1147)
+
+    return
 
     local_test_flag = False
+    do_login_flag = True
 
     login_id = "bora010"
     password = "bbb010"
-    opener = login(login_id, password, local_test_flag)
 
-    photo_no = 4
-    photo_key_enc, onetime_key = get_photo_param(opener, photo_no)
-    get_generator(opener, photo_no, photo_key_enc, onetime_key)
+    opener = login(login_id, password, local_test_flag, do_login_flag)
+
+    get_category_list(opener, 2)
+    download_pic(opener, group_id, 1, 1147)
 
     # get_category_list()
     print("end")
